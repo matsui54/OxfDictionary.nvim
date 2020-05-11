66e6c11da43b6be2883bf96a338a8d62c78bf548
Matsui54 <haru.matu9168@gmail.com>
2020-05-11 23:54:28 +0900

    overall changes

--- rplugin/python3/dict/__init__.py
+++ rplugin/python3/dict/__init__.py
@@ -16,13 +16,55 @@ class Dictionary:
             word = self.nvim.eval('expand("<cword>")')
         return word
 
+    def echo(self, *msgs):
+        self.nvim.command("echo '{}'".format(msgs))
+
     def re_definition(self,word):
         if word:
             return OxfordDict(self.app_id, self.app_key).get_definition(word)
         else:
-            return "Words are not selected."
+            return "Words are not selected.", -1
 
     @pynvim.command('Definition', nargs='*')
-    def definition(self, args=''):
+    def display_def(self, args=''):
         selectedW = self.get_word(args)
-        self.nvim.command('echo "{}"'.format(self.re_definition(selectedW)))
+        defs = self.re_definition(selectedW)
+
+        #If an error occurs, return error message.
+        if defs[1] == -1:
+            self.nvim.command("echo '{}'".format(defs[0]))
+            return
+
+        screen_height = int(self.nvim.command_output("echo &lines"))
+        screen_width = int(self.nvim.command_output("echo &columns"))
+        win_height = defs[1]
+        win_width = defs[2]
+        cWin_col = self.nvim.current.window.col
+        cWin_row = self.nvim.current.window.row
+        win_cursol = self.nvim.current.window.cursor
+
+        #where to set the new window
+        cursor_row = cWin_row+win_cursol[0] \
+            - int(self.nvim.command_output("echo line('w0')")) + 1
+        if cursor_row > screen_height/2:
+            anchor = 'SW'
+            fwin_row = max(int(win_height), cursor_row-1)
+        else:
+            anchor = 'NW'
+            fwin_row = min(screen_height-win_height, cursor_row+1)
+
+        #make floating window
+        self.nvim.command("let buf = nvim_create_buf(v:false, v:true)")
+        for meaning in defs[0]:
+            self.nvim.command("call nvim_buf_set_lines(buf, -1, -1, v:true, ['{}'])"\
+                              .format(meaning))
+        self.nvim.command("let opts = {{'relative': 'editor',\
+                                        'width': {0},\
+                                        'height': {1},\
+                                        'col': 5,\
+                                        'row': {2},\
+                                        'anchor': '{3}',\
+                                        'style': 'minimal'}}"\
+                          .format(win_width, win_height+1, fwin_row, anchor))
+        self.nvim.command("let win = nvim_open_win(buf, 1, opts)")
+
--- rplugin/python3/dict/dictionary.py
+++ rplugin/python3/dict/dictionary.py
@@ -10,27 +10,44 @@ class OxfordDict:
         self.app_key = app_key
         self.fields = 'definitions'
         self.strictMatch = 'false'
-        self.language = 'en-gb'
+        self.language = 'en-us'
 
     def get_definition(self, word_id):
         url = self.base_url + self.language + '/' + word_id.lower() + '?fields=' + self.fields + '&strictMatch=' + self.strictMatch;
 
         r = requests.get(url, headers = {'app_id': self.app_id, 'app_key': self.app_key})
-        json_load=json.loads(r.text)
 
-        jresults = json_load['results']
-        cntdef = 1
-        definitions = ''
-        for jresult in jresults:
-            jles = jresult['lexicalEntries']
-            for jle in jles:
-                ents = jle['entries']
-                for ent in ents:
-                    sens = ent['senses']
-                    for sen in sens:
-                        defns=sen['definitions']
-                        for defn in defns:
-                            definitions+=defn+'\n'
-                            cntdef+=1
+        if r.status_code == 200:
+            json_load=json.loads(r.text)
+        elif r.status_code == 404:
+            return 'No definition found', -1
+        else:
+            return 'Internal error', -1
 
-        return definitions.rstrip()
+        '''
+        #test____________________________
+        json_open = open('ace.json', 'r')
+        json_load = json.load(json_open)
+        #end test________________________
+        '''
+
+        definitions = []
+        cntcol=0
+        max_width = 10
+        for jresult in json_load['results']:
+            for jle in jresult['lexicalEntries']:
+                lCategory = jle['lexicalCategory']['text']
+                definitions.append(lCategory)
+                cntcol+=1
+                for ent in jle['entries']:
+                    for sen in ent['senses']:
+                        for defn in sen['definitions']:
+                            definitions.append('- '+defn)
+                            cntcol+=1
+                            max_width = max(max_width, min(len(defn)+2, 101))
+                            if len(defn)>100:
+                                cntcol+=1
+                                if len(defn)>199:
+                                    cntcol+=1
+        return definitions, cntcol, max_width
+
